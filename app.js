70695007981b15abc60c38a425a25191cbbb54c1
Anya <annagapyuk@gmail.com>
2020-06-12 20:34:36 -0400

    added to app.js

--- app.js
+++ app.js
@@ -5,4 +5,10 @@ let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
-btns.forEach(function () {});
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function(e) {
+        const styles = e.currentTarget.classList;
+
+    })
+
+});
