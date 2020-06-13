44158dc6b2cc6eb965a0960aecbd32bcd0fe5082
Anya <annagapyuk@gmail.com>
2020-06-13 12:08:50 -0400

    finished with app.js

--- app.js
+++ app.js
@@ -6,8 +6,27 @@ const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
-    btn.addEventListener('click', function(e) {
-        const styles = e.currentTarget.classList;
+    btn.addEventListener('click', function(e) { // e = event object
+        const styles = e.currentTarget.classList; // current target at the classList
+        if (styles.contains('decrease')) {
+            count--;
+        } else if (styles.contains('increase')) {
+            count++;
+        } else {
+            count = 0;
+        }
+        // change colors
+       if (count > 0) {
+           value.style.color = "green";
+
+       } if (count < 0) {
+           value.style.color = "red";
+       }
+       if (count == 0) {
+           value.style.color = "#222";
+       }
+
+        value.textContent = count;
 
     })
 
